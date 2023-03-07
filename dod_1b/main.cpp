f1f07cc16f432c74927bbe4d69cec3fbf79c556d
Daniel Goodnow <danielgoodnow@gmail.com>
2023-03-07 05:44:21 -0800

    Cleanup

--- dod_1b/main.cpp
+++ dod_1b/main.cpp
@@ -3,8 +3,9 @@
 #include <cstring>
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
-const int ITERATIONS = 8000000;
+const int ITERATIONS = 1000000;
 
 static std::array<int, ITERATIONS> intArray;
 class ArrayFixture : public benchmark::Fixture
@@ -19,59 +20,54 @@ public:
    }
 };
 
-class ListNode {
+class NamedInt {
 public:
-   ListNode* Next;
    int Value;
-   ListNode(int value, ListNode* next = nullptr) 
+   std::string Name;
+   NamedInt() {
+      Value = 0;
+      Name = "";
+   }
+   NamedInt(int value, std::string name)
    {
-      Value = value;
-      Next = next;
+      this->Value = value;
+      this->Name = name;
    }
 };
-static ListNode* head = nullptr;
-class ListFixture : public benchmark::Fixture
+
+static std::array<NamedInt, ITERATIONS> namedIntArray;
+class ObjectFixture : public benchmark::Fixture
 {
 public:
    void SetUp(const ::benchmark::State& state)
    {
-      head = new ListNode(2);
-      auto current = head;
       for(int i = 0; i < ITERATIONS; i++)
       {
-         current->Next = new ListNode(2);
-         current = current->Next;
+         namedIntArray[i] = NamedInt(2, "Named Int");
       }
    }
    void TearDown(const ::benchmark::State& state)
    {
-      auto current = head;
-      while(current != nullptr)
-      {
-         auto temp = current;
-         current = current->Next;
-         delete(temp);
-      }
    }
 };
 
 BENCHMARK_F(ArrayFixture, ArrayTest)(benchmark::State& st)
 {
    for(auto _ : st) {
-      //benchmark::ClobberMemory();
+      benchmark::ClobberMemory();
       for(int i = 0; i < intArray.size(); i++) {
          benchmark::DoNotOptimize(intArray[i] *= intArray[i]);
       }
    }
 }
 
-BENCHMARK_F(ListFixture, ListTest)(benchmark::State& st)
+BENCHMARK_F(ObjectFixture, ObjectTest)(benchmark::State& st)
 {
    for(auto _ : st) {
-      //benchmark::ClobberMemory();
-      for(ListNode* current = head; current != nullptr; current = current->Next)
+      benchmark::ClobberMemory();
+      for(int i = 0; i < ITERATIONS; i++)
       {
-         benchmark::DoNotOptimize(current->Value *= current->Value);
+         benchmark::DoNotOptimize(namedIntArray[i].Value *= namedIntArray[i].Value);
       }
    }
 }
